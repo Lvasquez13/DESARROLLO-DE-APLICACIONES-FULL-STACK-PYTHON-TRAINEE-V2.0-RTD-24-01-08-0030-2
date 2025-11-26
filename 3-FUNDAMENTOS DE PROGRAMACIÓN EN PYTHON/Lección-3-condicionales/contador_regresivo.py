7b6733e0b25450503d262fb904ac79403ca809c7
Luis Vásquez <luis.avasquezvillaroel@gmail.com>
2025-11-25 22:10:27 -0300

    Actividades_m3_l3

--- 3-FUNDAMENTOS DE PROGRAMACIÓN EN PYTHON/Lección-3-condicionales/contador_regresivo.py
+++ 3-FUNDAMENTOS DE PROGRAMACIÓN EN PYTHON/Lección-3-condicionales/contador_regresivo.py
@@ -1,11 +1,16 @@
-#  contador regresivo
-numero=int(input("Ingrese un numero:"))
-descuento=0
-contador_regresivo=numero
+# contador regresivo
 
-while descuento<numero:
-  print(f"Contador regresivo {contador_regresivo}")
-  descuento=descuento+1
-  contador_regresivo=contador_regresivo-1
+numero = int(input("Ingrese un numero (negativo para salir): "))
 
-print(f"Contador regresivo {contador_regresivo}")
+while numero >= 0:     # mientras sea positivo o cero
+    descuento = 0
+    contador_regresivo = numero
+    
+    # contador regresivo
+    while descuento <= numero:
+        print(f"Contador regresivo: {contador_regresivo}")
+        descuento += 1
+        contador_regresivo -= 1
+
+    # pedir otro número
+    numero = int(input("Ingrese otro número (negativo para salir): "))
